dc5344c6f55f0e1519cab1c62d615d152163f503
Bjornar <bjornar.rh@gmail.com>
2022-09-27 18:37:55 +0200

    Simple PD controller for y-direction with subscriber for reference point.

---
+++ flappy_automation_code/include/flappy_automation_code/PD_controller.hpp
@@ -0,0 +1,33 @@
+#ifndef PD_CONTROLLER_H_
+#define PD_CONTROLLER_H_
+#include <ros/ros.h>
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Vector3.h"
+
+
+class PDController
+{
+public:
+    PDController(ros::NodeHandlePtr nh);
+    ~PDController();
+
+private:
+    const float KP = 25;
+    const float KD = 9;
+    const float T = 1.0/30.0;
+
+    ros::Subscriber sub_vel;
+    ros::Subscriber sub_ref_pos;
+    ros::Publisher pub_acc;
+    
+    geometry_msgs::Vector3 flappy_reference_position;
+    geometry_msgs::Vector3 flappy_position;
+    geometry_msgs::Vector3 flappy_velocity;
+    geometry_msgs::Vector3 previous_error_value;
+    geometry_msgs::Vector3 acc_cmd;
+
+    void refPosSub(const geometry_msgs::Vector3::ConstPtr& msg);
+    void velSubPDPub(const geometry_msgs::Vector3::ConstPtr& msg);
+};
+
+#endif
---
+++ flappy_automation_code/src/PD_controller.cpp
@@ -0,0 +1,32 @@
+#include "ros/ros.h"
+#include "flappy_automation_code/PD_controller.hpp"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Vector3.h"
+
+
+PDController::PDController(ros::NodeHandlePtr nh) : 
+    sub_vel{nh->subscribe<geometry_msgs::Vector3>("/flappy_vel", 1, &PDController::velSubPDPub, this)},
+    sub_ref_pos{nh->subscribe<geometry_msgs::Vector3>("/flappy_ref_pos", 1, &PDController::refPosSub, this)},
+    pub_acc{nh->advertise<geometry_msgs::Vector3>("/flappy_acc", 1)}
+{
+    flappy_reference_position = geometry_msgs::Vector3();
+    flappy_position = geometry_msgs::Vector3();
+    previous_error_value = geometry_msgs::Vector3();
+}
+PDController::~PDController()
+{
+}
+void PDController::refPosSub(const geometry_msgs::Vector3::ConstPtr& msg){
+    flappy_reference_position = *msg;
+}
+
+void PDController::velSubPDPub(const geometry_msgs::Vector3::ConstPtr& msg){
+    geometry_msgs::Vector3 velocity_measurement = *msg;
+    flappy_position.y += velocity_measurement.y*T;
+
+    float error_value = (flappy_reference_position.y - flappy_position.y);
+    acc_cmd.y = KP*error_value + KD*(0.0 - velocity_measurement.y);
+    previous_error_value.y = error_value;
+
+    pub_acc.publish(acc_cmd);
+}
