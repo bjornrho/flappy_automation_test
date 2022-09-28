5173798722825378e30e0a31b39cc53dccf4d25d
Bjornar <bjornar.rh@gmail.com>
2022-09-28 16:40:13 +0200

    Added PD controller for velocity in x. Not tested.

--- flappy_automation_code/include/flappy_automation_code/PD_controller.hpp
+++ flappy_automation_code/include/flappy_automation_code/PD_controller.hpp
@@ -12,16 +12,22 @@ public:
     ~PDController();
 
 private:
-    const float KP = 30;
-    const float KD = 10;
+    const float KP_POS = 30;
+    const float KD_POS = 10;
+    const float KP_VEL = 30;
+    const float KD_VEL = 10;
+    const float T = 1.0/30.0;
 
     ros::Subscriber sub_vel;
     ros::Subscriber sub_ref_pos;
     ros::Publisher pub_acc;
     
-    geometry_msgs::Vector3 flappy_reference_position;
+    float flappy_reference_position_y;
+    float flappy_reference_velocity_x;
+    float flappy_previous_velocity_x;
 
     float YPositionControl(geometry_msgs::Vector3 velocity_measurement);
+    float XVelocityControl(geometry_msgs::Vector3 velocity_measurement);
     void refPosSub(const geometry_msgs::Vector3::ConstPtr& msg);
     void velSubPDPub(const geometry_msgs::Vector3::ConstPtr& msg);
     
--- flappy_automation_code/src/PD_controller.cpp
+++ flappy_automation_code/src/PD_controller.cpp
@@ -6,22 +6,29 @@
 
 PDController::PDController(ros::NodeHandlePtr nh) : 
     sub_vel{nh->subscribe<geometry_msgs::Vector3>("/flappy_vel", 1, &PDController::velSubPDPub, this)},
-    sub_ref_pos{nh->subscribe<geometry_msgs::Vector3>("/flappy_ref_pos", 1, &PDController::refPosSub, this)},
+    sub_ref_pos{nh->subscribe<geometry_msgs::Vector3>("/flappy_reference", 1, &PDController::refPosSub, this)},
     pub_acc{nh->advertise<geometry_msgs::Vector3>("/flappy_acc", 1)}
 {
-    flappy_reference_position = geometry_msgs::Vector3();
-}
-PDController::~PDController()
-{
+    flappy_previous_velocity_x = 0.0;
 }
 
+PDController::~PDController(){}
+
 float PDController::YPositionControl(geometry_msgs::Vector3 velocity_measurement){
-    return KP*flappy_reference_position.y - KD*velocity_measurement.y;
+    return KP_POS*flappy_reference_position_y - KD_POS*velocity_measurement.y;
+}
+
+float PDController::XVelocityControl(geometry_msgs::Vector3 velocity_measurement){
+    float acceleration_x = (velocity_measurement.x - flappy_previous_velocity_x) / T;
+    flappy_previous_velocity_x = velocity_measurement.x;
+
+    return (KP_VEL*flappy_reference_velocity_x - KD_VEL*acceleration_x);
 }
 
 
 void PDController::refPosSub(const geometry_msgs::Vector3::ConstPtr& msg){
-    flappy_reference_position = *msg;
+    flappy_reference_position_y = msg->y;
+    flappy_reference_velocity_x = msg->x;
 }
 
 void PDController::velSubPDPub(const geometry_msgs::Vector3::ConstPtr& msg){
