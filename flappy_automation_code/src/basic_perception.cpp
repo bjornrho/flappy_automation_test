82d43a23cf82655d4c59a19b83c6b1e4728f9852
Bjornar <bjornar.rh@gmail.com>
2022-09-29 16:40:44 +0200

    Removed unused outlier rejection.

--- flappy_automation_code/src/basic_perception.cpp
+++ flappy_automation_code/src/basic_perception.cpp
@@ -8,31 +8,6 @@
 #include <math.h>
 
 /* ====== HELPER FUNCTIONS ====== */
-int median(int l, int r)
-{
-    int n = r - l + 1;
-    n = (n + 1) / 2 - 1;
-    return n + l;
-}
- 
-std::vector<float> IQR(std::vector<float> a, int n)
-{
-    std::vector<float> limits;
-    sort(a.begin(), a.end());
-    int mid_index = median(0, n);
-
-    float Q1 = a[median(0, mid_index)];
-    float Q3 = a[mid_index + median(mid_index + 1, n)];
-    float IQR = (Q3 - Q1);
-
-    float lower_limit = Q1 - 1.5*IQR;
-    float upper_limit = Q3 + 1.5*IQR;
-    limits.push_back(lower_limit);
-    limits.push_back(upper_limit);
-
-    return limits;
-}
-
 float meanOfVector(std::vector<float> data_vector){
     if(data_vector.empty()){
         return -1;
@@ -46,35 +21,7 @@ float meanOfVector(std::vector<float> data_vector){
     return sum / data_vector.size();
 }
 
-float outlierRejectionSTD(std::vector<float> data_vector, float data_vector_mean){
-    float sum = 0.0;
-    for(auto i : data_vector){
-        sum += std::pow((i-data_vector_mean),2);
-    }
-    float std = std::sqrt(sum/data_vector.size());
-    sum = 0.0;
-    int count = 0;
-    for(auto i : data_vector){
-        if(i < (data_vector_mean + 0.5*std) || i > (data_vector_mean - 0.5*std)){
-            sum += i;
-            count += 1;
-        }
-    }
-    return sum / float(count);
-}
-
-float outlierRejectionIQR(std::vector<float> data_vector){
-    float sum = 0.0;
-    int count = 0;
-    std::vector<float> limits = IQR(data_vector, data_vector.size());
-    for(auto i : data_vector){
-        if(i > limits[0] && i < limits[1]){
-            sum += (float)i;
-            count += 1;
-        }
-    }
-    return sum / count;
-}
+/* ====== BasicPerception CLASS ====== */
 
 BasicPerception::BasicPerception(ros::NodeHandlePtr nh) : 
     sub_laser_scan{nh->subscribe<sensor_msgs::LaserScan>("/flappy_laser_scan", 1, &BasicPerception::laserSubRefencePub, this)},
@@ -89,20 +36,20 @@ BasicPerception::~BasicPerception(){
 
 void BasicPerception::laserSubRefencePub(const sensor_msgs::LaserScan::ConstPtr& msg){
     flappy_laser_scan = *msg;
+
     float wall_position = estimateClosestWallPosition();
-    ROS_INFO("Estimated wall placement: %f", wall_position);
     float ref_pos_y = estimateReferencePositionY(wall_position);
     float ref_vel_x = estimateReferenceVelocityX(wall_position);
-    geometry_msgs::Vector3 ref = geometry_msgs::Vector3();
     
+    geometry_msgs::Vector3 ref = geometry_msgs::Vector3();
     if((wall_position != -1) && (previous_wall_position != -1) && (wall_position - previous_wall_position) > 1.5){
         ref.y = 0.0;
         ref.x = TOP_SPEED;
     }else{
         ref.y = ref_pos_y;
+        ref.x = ref_vel_x;
     }
 
-    ref.x = ref_vel_x;
     pub_ref_pos.publish(ref);
 }
 
@@ -145,20 +92,6 @@ float BasicPerception::estimateReferencePositionY(float wall_position){
     }
 
     float mean = meanOfVector(possible_reference_points_y);
-
-    // Outlier rejection using Interquartile Range (IQR)
-    //if(possible_reference_points_y.size() > 1){
-    //    float outliersRemoved = outlierRejectionIQR(possible_reference_points_y);
-    //    if(outliersRemoved > 0.0){
-    //        mean = outliersRemoved;
-    //    }
-    //}
-
-
-    // Simple outlier rejection using std
-    //if(possible_reference_points_y.size() > 1){
-    //    mean = outlierRejectionSTD(possible_reference_points_y, mean);
-    //}
     
     return mean;
 }
