69d6ccf3128502a985fc5cf10c7a0cab8216a244
Bjornar <bjornar.rh@gmail.com>
2022-09-29 15:34:08 +0200

    Added gate passing logic. Stable but slow performance. 40+ per run.

--- flappy_automation_code/include/flappy_automation_code/basic_perception.hpp
+++ flappy_automation_code/include/flappy_automation_code/basic_perception.hpp
@@ -13,20 +13,21 @@ public:
 
 private:
     float WALL_WIDTH = 1.0;
-    float GATE_OPENING = 1.5;
-    float SURGE_SPEED = 4.0;
-    float EMERGENCY_BREAK = 1.6;
+    float TOP_SPEED = 4.0;
+    float EMERGENCY_BREAK = 2.0;
 
     ros::Subscriber sub_laser_scan;
     ros::Publisher pub_ref_pos;
     ros::Publisher pub_acc;
 
     sensor_msgs::LaserScan flappy_laser_scan;
+    float flappy_reference_position_y;
+    float previous_wall_position;
 
     void laserSubRefencePub(const sensor_msgs::LaserScan::ConstPtr& msg);
     float estimateClosestWallPosition();
-    float estimateReferencePositionY(const float wall_estimate);
-    float estimateReferenceVelocityX();
+    float estimateReferencePositionY(const float wall_position);
+    float estimateReferenceVelocityX(const float wall_position);
 };
 
 #endif
--- flappy_automation_code/src/basic_perception.cpp
+++ flappy_automation_code/src/basic_perception.cpp
@@ -7,27 +7,73 @@
 #include <vector>
 #include <math.h>
 
+/* ====== HELPER FUNCTIONS ====== */
+int median(int l, int r)
+{
+    int n = r - l + 1;
+    n = (n + 1) / 2 - 1;
+    return n + l;
+}
+ 
+std::vector<float> IQR(std::vector<float> a, int n)
+{
+    std::vector<float> limits;
+    sort(a.begin(), a.end());
+    int mid_index = median(0, n);
 
+    float Q1 = a[median(0, mid_index)];
+    float Q3 = a[mid_index + median(mid_index + 1, n)];
+    float IQR = (Q3 - Q1);
+
+    float lower_limit = Q1 - 1.5*IQR;
+    float upper_limit = Q3 + 1.5*IQR;
+    limits.push_back(lower_limit);
+    limits.push_back(upper_limit);
+
+    return limits;
+}
+
+float meanOfVector(std::vector<float> data_vector){
+    if(data_vector.empty()){
+        return -1;
+    }
+
+    float sum = 0.0;
+    for(auto data_point : data_vector){
+        sum += (float)data_point;
+    }
+
+    return sum / data_vector.size();
+}
 
 BasicPerception::BasicPerception(ros::NodeHandlePtr nh) : 
     sub_laser_scan{nh->subscribe<sensor_msgs::LaserScan>("/flappy_laser_scan", 1, &BasicPerception::laserSubRefencePub, this)},
     pub_ref_pos{nh->advertise<geometry_msgs::Vector3>("/flappy_reference", 1)},
     pub_acc{nh->advertise<geometry_msgs::Vector3>("/flappy_acc", 1)}
 {
+    previous_wall_position = 0.0;
 }
 BasicPerception::~BasicPerception(){
 }
 
+
 void BasicPerception::laserSubRefencePub(const sensor_msgs::LaserScan::ConstPtr& msg){
     flappy_laser_scan = *msg;
     float wall_position = estimateClosestWallPosition();
+    ROS_INFO("Estimated wall placement: %f", wall_position);
     float ref_pos_y = estimateReferencePositionY(wall_position);
-    float ref_vel_x = estimateReferenceVelocityX();
+    float ref_vel_x = estimateReferenceVelocityX(wall_position);
     geometry_msgs::Vector3 ref = geometry_msgs::Vector3();
-    ref.y = ref_pos_y;
+    
+    if((wall_position != -1) && (previous_wall_position != -1) && (wall_position - previous_wall_position) > 1.5){
+        ref.y = 0.0;
+        ref.x = TOP_SPEED;
+    }else{
+        ref.y = ref_pos_y;
+    }
+
     ref.x = ref_vel_x;
     pub_ref_pos.publish(ref);
-
 }
 
 float BasicPerception::estimateClosestWallPosition(){
@@ -49,84 +95,65 @@ float BasicPerception::estimateClosestWallPosition(){
         }
     }
 
-    if(valid_x_distance.empty()){
-        return -1;
-    }
-
-    float sum = 0.0;
-    for(auto i : valid_x_distance){
-        sum += (float)i;
-    }
-    float avg =  sum / valid_x_distance.size();
-
-    
-    ROS_INFO("Estimated wall placement: %f", avg);
-    //std::stringstream ss;
-    //ss << "Data Retrieved: \n";
-    //std::copy(valid_x_distance.begin(), valid_x_distance.end(), std::ostream_iterator<double>(ss, " "));
-    //ss << std::endl;
-    //ROS_INFO_STREAM(ss.str());
-    
-
-    return avg;
+    return meanOfVector(valid_x_distance);
 }
 
-float BasicPerception::estimateReferencePositionY(float wall_estimate){
-    if (wall_estimate == -1){
+float BasicPerception::estimateReferencePositionY(float wall_position){
+    if (wall_position == -1){
         return 0.0;
     }
+
     int number_of_rays = flappy_laser_scan.ranges.size();
     std::vector<float> possible_reference_points_y;
 
     for(int ray_n = 0; ray_n < number_of_rays; ray_n++){
-        if (flappy_laser_scan.ranges[ray_n] > (wall_estimate + WALL_WIDTH)){
+        if (flappy_laser_scan.ranges[ray_n] > (wall_position + WALL_WIDTH)){
             float scan_angle = flappy_laser_scan.angle_min + ray_n*flappy_laser_scan.angle_increment;
             float y_reference = flappy_laser_scan.ranges[ray_n]*sin(scan_angle);
             possible_reference_points_y.push_back(y_reference);
         }
     }
 
-    if (possible_reference_points_y.empty()){
-        return 0.0;
-    }
-    
-    float sum = 0.0;
-    for(auto i : possible_reference_points_y){
-        sum += (float)i;
-    }
-    float mean =  sum / possible_reference_points_y.size();
+    float mean = meanOfVector(possible_reference_points_y);
+
+    // Simple outlier rejection using std
+    //sum = 0.0;
+    //int count = 0;
+    //std::vector<float> limits = IQR(possible_reference_points_y, possible_reference_points_y.size());
+    //for(auto i : possible_reference_points_y){
+    //    if(i > limits[0] && i < limits[1]){
+    //        sum += (float)i;
+    //        count += 1;
+    //    }
+    //}
+    //if(sum != 0.0){
+    //    return (sum / count);
+    //}
 
 
-    if(possible_reference_points_y.size() >= 2){
-        sum = 0.0;
-        for(auto i : possible_reference_points_y){
-            sum += std::pow((i-mean),2);
-        }
-        float std = std::sqrt(sum/possible_reference_points_y.size());
-        ROS_INFO("STD: %f", std);
-        sum = 0.0;
-        int count = 0;
-        for(auto i : possible_reference_points_y){
-            if(i < (mean + 0.5*std) || i > (mean - 0.5*std)){
-                sum += i;
-                count += 1;
-            }
-        }
-        float avg = sum / float(count);
-        ROS_INFO("Estimated ref point: %f", avg);
-        std::stringstream ss;
-        ss << "Data Retrieved: \n";
-        std::copy(possible_reference_points_y.begin(), possible_reference_points_y.end(), std::ostream_iterator<double>(ss, " "));
-        ss << std::endl;
-        ROS_INFO_STREAM(ss.str());
-        return avg;
-    }
+    // Outlier rejection using Interquartile Range (IQR)
+    //if(possible_reference_points_y.size() >= 2){
+    //    sum = 0.0;
+    //    for(auto i : possible_reference_points_y){
+    //        sum += std::pow((i-mean),2);
+    //    }
+    //    float std = std::sqrt(sum/possible_reference_points_y.size());
+    //    sum = 0.0;
+    //    int count = 0;
+    //    for(auto i : possible_reference_points_y){
+    //        if(i < (mean + 0.5*std) || i > (mean - 0.5*std)){
+    //            sum += i;
+    //            count += 1;
+    //        }
+    //    }
+    //    return sum / float(count);
+    //}
     
     return mean;
 }
 
-float BasicPerception::estimateReferenceVelocityX(){
-    if(flappy_laser_scan.ranges[5] < EMERGENCY_BREAK){
+float BasicPerception::estimateReferenceVelocityX(const float wall_position){
+    if(flappy_laser_scan.ranges[4] < EMERGENCY_BREAK || flappy_laser_scan.ranges[5] < EMERGENCY_BREAK || flappy_laser_scan.ranges[3] < EMERGENCY_BREAK){
         return 0.0;
     }
     return TOP_SPEED;
